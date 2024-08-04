56156d5925d4071e37245010b3d83c66f26c0e04
SagarK107 <sagarkarambelkar107@gmail.com>
2024-08-04 08:18:44 +0530

    Can perform operations on two numbers

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 
 const calculator_data = {num1 : 0, num2 : 0, operation : "", currentNumber : "1"}
 
-function numberInput(num,whichNumber)
+function numberInput(num)
 {
-    if (whichNumber === "1")
+    
+    if (calculator_data.currentNumber === "1")
     {
-        calculator_data.num1 = num;
-        calculator_data.currentNumber = "2";
+        calculator_data.num1 = (calculator_data.num1 * 10) + num;
+        
     }
 
-    else if (whichNumber === "2")
+    else if (calculator_data.currentNumber === "2")
     {
-        calculator_data.num2 = num;
-        calculator_data.currentNumber = "1";
+        calculator_data.num2 = (calculator_data.num2 * 10) + num;
+        
     }
 
     else
     {
         console.log("Error!");
     }
+    console.log(calculator_data);
 }
+
+function operatorInput(op)
+{
+    const operations = new Set(["+","-","*","/"]);
+    console.log(op);
+
+    if (operations.has(op))
+    {
+        calculator_data.operation = op;
+        if (calculator_data.currentNumber == "1")
+        {
+            calculator_data.currentNumber = "2";
+        }
+
+        else if (calculator_data.currentNumber == "2")
+        {
+            calculator_data.currentNumber = "1";
+        }
+    }
+
+}
+
 
 function operation(num1,num2,operation)
 {
@@ -40,6 +64,17 @@ function operation(num1,num2,operation)
         default:
             return NaN;
     }
+}
+
+function evaluate_expn()
+{
+    //TODO: Handle case where the result of the operation is NaN
+    console.log(calculator_data);
+    calculator_data.num1 = operation(calculator_data.num1,calculator_data.num2,calculator_data.operation); 
+    calculator_data.num2 = 0;
+    calculator_data.currentNumber = "1";
+    console.log(calculator_data.num1);
+    document.getElementById("result").textContent = calculator_data.num1;
 }
 
 
